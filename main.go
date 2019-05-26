b16c51760e5d45a7b4731f6cdf4e898fb456f5ee
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-05-26 12:29:11 +0300

    Implement basic health endpoint with Gorilla Mux

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello GS!")
+	router := mux.NewRouter()
+	router.HandleFunc("/health", Health).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+// Health the health endpoint provides basic application health information
+func Health(w http.ResponseWriter, r *http.Request) {
+	log.Println("Health endpoint")
+
+	healthStruct := struct {
+		AppStatus string `json:"app_status"`
+	}{
+		"ok",
+	}
+
+	response, err := json.Marshal(healthStruct)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		log.Println("Error while marshaling health struct", err.Error())
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(response))
 }
