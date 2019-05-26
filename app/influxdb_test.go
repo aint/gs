264f398a0c50de9307cfd60e9976cc6261e34186
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-05-26 19:47:25 +0300

    Add tests for influxDB client

---
+++ app/influxdb_test.go
@@ -0,0 +1,145 @@
+package app
+
+import (
+	"fmt"
+	influx "github.com/influxdata/influxdb1-client/v2"
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+	"time"
+)
+
+func TestFetchByType(t *testing.T) {
+	Convey("Given start, end and type parameters", t, func() {
+		eventType := "session_start"
+		start := int64(1558888555)
+		end := int64(1558888888)
+
+		Convey("Happy case", func() {
+			// given
+			response := &influx.Response{Results: []influx.Result{}, Err: ""}
+
+			influxClient := new(MockedInfluxClient)
+			influxClient.On("Query", mock.Anything).Return(response, nil)
+			influxClient.On("Close").Return(nil)
+
+			dbClient := NewInfluxDBClient(influxClient)
+
+			// when
+			events, err := dbClient.FetchByType(eventType, start, end)
+
+			// then
+			assert.NoError(t, err)
+			assert.Empty(t, events)
+
+			influxClient.AssertExpectations(t)
+		})
+
+		Convey("Fail case", func() {
+			// given
+			response := &influx.Response{Results: []influx.Result{}, Err: "Some error on DB read"}
+
+			influxClient := new(MockedInfluxClient)
+			influxClient.On("Query", mock.Anything).Return(response, nil)
+			influxClient.On("Close").Return(nil)
+
+			dbClient := NewInfluxDBClient(influxClient)
+
+			// when
+			events, err := dbClient.FetchByType(eventType, start, end)
+
+			// then
+			assert.Error(t, err)
+			assert.Empty(t, events)
+
+			influxClient.AssertExpectations(t)
+		})
+	})
+}
+
+func TestSave(t *testing.T) {
+	Convey("Given some event", t, func() {
+		event := EventModel{"type", 123456, map[string]interface{}{"key": 42}}
+
+		Convey("Happy case", func() {
+			// given
+			bps, _ := influx.NewBatchPoints(influx.BatchPointsConfig{Database: "mydb"})
+			point, _ := constructPoint(event)
+			bps.AddPoint(point)
+
+			influxClient := new(MockedInfluxClient)
+			influxClient.On("Write", bps).Return(nil)
+			influxClient.On("Close").Return(nil)
+
+			dbClient := NewInfluxDBClient(influxClient)
+
+			// when
+			err := dbClient.Save(event)
+
+			// then
+			assert.NoError(t, err)
+
+			influxClient.AssertExpectations(t)
+		})
+
+		Convey("Fail case", func() {
+			// given
+			influxClient := new(MockedInfluxClient)
+			influxClient.On("Write", mock.Anything).Return(fmt.Errorf("Some error on DB write"))
+			influxClient.On("Close").Return(nil)
+
+			dbClient := NewInfluxDBClient(influxClient)
+
+			// when
+			err := dbClient.Save(event)
+
+			// then
+			assert.Error(t, err)
+
+			influxClient.AssertExpectations(t)
+		})
+	})
+}
+
+func constructPoint(event EventModel) (*influx.Point, error) {
+	tags := map[string]string{"event_type": event.EventType}
+
+	fields := make(map[string]interface{})
+	for k, v := range event.Params {
+		fields[k] = v
+	}
+
+	tm := time.Unix(event.Ts, 0)
+
+	return influx.NewPoint("events", tags, fields, tm)
+}
+
+type MockedInfluxClient struct {
+	mock.Mock
+}
+
+func (m *MockedInfluxClient) Ping(timeout time.Duration) (time.Duration, string, error) {
+	args := m.Called(timeout)
+	return args.Get(0).(time.Duration), args.String(1), args.Error(2)
+}
+
+func (m *MockedInfluxClient) Write(bp influx.BatchPoints) error {
+	args := m.Called(bp)
+	return args.Error(0)
+}
+
+func (m *MockedInfluxClient) Query(q influx.Query) (*influx.Response, error) {
+	args := m.Called(q)
+	return args.Get(0).(*influx.Response), args.Error(1)
+}
+
+func (m *MockedInfluxClient) QueryAsChunk(q influx.Query) (*influx.ChunkedResponse, error) {
+	args := m.Called(q)
+	return args.Get(0).(*influx.ChunkedResponse), args.Error(1)
+}
+
+func (m *MockedInfluxClient) Close() error {
+	args := m.Called()
+	return args.Error(0)
+}
