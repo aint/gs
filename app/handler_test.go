bb45be5b67a1d0e4de1f1eb1887c91d067c9b5f0
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-05-27 14:33:24 +0300

    Add happy tests cases for get and save events handler methods

--- app/handler_test.go
+++ app/handler_test.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -48,6 +49,74 @@ func TestHealth(t *testing.T) {
 			assert.Equal(t, http.StatusInternalServerError, rr.Code, "handler returned wrong status code")
 
 			expected := `{"error":"Error while pinging DB: 'can't connect to DB'"}`
+			assert.Equal(t, expected, rr.Body.String(), "handler returned unexpected body")
+
+			mockedDBClient.AssertExpectations(t)
+		})
+	})
+}
+
+func TestSaveEvents(t *testing.T) {
+	t.Run("Given a request and body for save events endpoint", func(t *testing.T) {
+		body := `[{
+			"event_type": "link_clicked",
+			"ts": 1558892660,
+			"params": { "url": "localhost:5000/app" }
+		},
+		{
+			"event_type": "link_clicked",
+			"ts": 1558892797,
+			"params": { "url": "localhost:6000/app" }
+		}]`
+		req, err := http.NewRequest(http.MethodPost, "/events", bytes.NewBuffer([]byte(body)))
+		assert.NoError(t, err)
+
+		t.Run("Happy case", func(t *testing.T) {
+			// given
+			mockedDBClient := new(MockedInfluxDBClient)
+			mockedDBClient.On("Save", mock.Anything).Return(nil)
+
+			rr := httptest.NewRecorder()
+
+			// when
+			SaveEvents(mockedDBClient, rr, req)
+
+			// then
+			assert.Equal(t, http.StatusCreated, rr.Code, "handler returned wrong status code")
+			assert.Equal(t, "", rr.Body.String(), "handler returned unexpected body")
+
+			mockedDBClient.AssertExpectations(t)
+		})
+	})
+}
+
+func TestGetEvents(t *testing.T) {
+	t.Run("Given a request with params to get events endpoint", func(t *testing.T) {
+		eventType := "session_created"
+		start := 10
+		end := 5
+		url := fmt.Sprintf("/events/relative?type=%s&start=%dm&end=%dm", eventType, start, end)
+		req, err := http.NewRequest(http.MethodPost, url, nil)
+		assert.NoError(t, err)
+
+		t.Run("Happy case", func(t *testing.T) {
+			// given
+			events := []EventModel{
+				EventModel{eventType, int64(1558892660), map[string]interface{}{"key1": "val1"}},
+				EventModel{eventType, int64(1558892797), map[string]interface{}{"key2": "val2"}},
+			}
+			mockedDBClient := new(MockedInfluxDBClient)
+			mockedDBClient.On("FetchByType", eventType, int64(60 * start), int64(60 * end)).Return(events, nil)
+
+			rr := httptest.NewRecorder()
+
+			// when
+			GetEvents(mockedDBClient, rr, req)
+
+			// then
+			assert.Equal(t, http.StatusOK, rr.Code, "handler returned wrong status code")
+
+			expected := "[{\"event_type\":\"session_created\",\"ts\":1558892660,\"params\":{\"key1\":\"val1\"}},{\"event_type\":\"session_created\",\"ts\":1558892797,\"params\":{\"key2\":\"val2\"}}]"
 			assert.Equal(t, expected, rr.Body.String(), "handler returned unexpected body")
 
 			mockedDBClient.AssertExpectations(t)
